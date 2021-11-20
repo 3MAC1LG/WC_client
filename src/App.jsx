00dcdaf0e03be3bf3022cabd16ae216f1f8720da
JunSeo <oponize@naver.com>
2021-11-20 15:24:19 +0900

    [Router] 라우터 접근 권한 수정 - 라우터

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Switch } from 'react-router';
 import useDarkMode from 'use-dark-mode';
 import GlobalStyles from './lib/styles/global';
@@ -9,12 +9,18 @@ import Main from './pages/Main';
 import Landing from './pages/Landing';
 import Header from './layout/Header';
 import useSWR from 'swr';
+import { fetcher } from './lib/api/fetcher';
 
 const Login = loadable(() => import('./pages/Login'));
 const Account = loadable(() => import('./pages/Account'));
+const MyPage = loadable(() => import('./pages/MyPage'));
 const Lecture = loadable(() => import('./pages/Lecture'));
 
 const App = () => {
+  const { data: userData, error } = useSWR(
+    'http://localhost:4000/api/users',
+    fetcher,
+  );
   const darkMode = useDarkMode(false);
   return (
     <>
@@ -25,18 +31,24 @@ const App = () => {
           <Route exact path="/">
             <Main />
           </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
+          <Route
+            path="/login"
+            render={(props) => !userData && <Login {...props} />}
+          />
           <Route path="/landing">
             <Landing />
           </Route>
           <Route path="/lecture">
             <Lecture />
           </Route>
-          <Route path="/account">
-            <Account />
-          </Route>
+          <Route
+            path="/mypage"
+            render={(props) => userData && <MyPage {...props} />}
+          />
+          <Route
+            path="/account"
+            render={(props) => userData && <Account {...props} />}
+          />
         </Switch>
       </ThemeProvider>
     </>
--- src/Pages/Account/index.jsx
+++ src/Pages/Account/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { AccountContainer } from './styles';
+import useSWR from 'swr';
 
 const Accout = () => {
+  // const {} = useSWR(`http://localhost:4000/api/classrooms/:${}`)
   return (
-    <div>
+    <AccountContainer>
       <h1>Acccount</h1>
-    </div>
+    </AccountContainer>
   );
 };
 
