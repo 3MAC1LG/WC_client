22185c7bea3731776114ef0fff945bd4f3ac9306
Somin Oh <smhs12@naver.com>
2021-11-22 15:43:52 +0900

    [UI] 강의찾기 페이지 다크모드 적용 - 강의찾기 페이지

--- src/Pages/Lecture/LectureList/index.jsx
+++ src/Pages/Lecture/LectureList/index.jsx
@@ -1,10 +1,22 @@
+/** @jsxImportSource @emotion/react */
+import { useTheme } from '@emotion/react';
 import React from 'react';
-import { ListContainer, SearchBox } from './styles';
+import { ContainerStyles, ListContainer, SearchBox } from './styles';
 import { AiOutlineSearch } from 'react-icons/ai';
 import LectureItem from '../../../components/LectureItem';
+import Select from 'react-select';
+
 const LectureList = () => {
+  const theme = useTheme();
+  const options = [{ value: '프론트엔드', label: 'front' }];
+  const customStyles = {
+    container: (provided, state) => ({
+      ...provided,
+      borderRadius: '7rem',
+    }),
+  };
   return (
-    <ListContainer>
+    <ListContainer css={ContainerStyles(theme)}>
       <div className="title">
         요즘 뜨고 있는 <span>개발자</span>, 나도 도전 할 수 있을까?
       </div>
@@ -14,11 +26,7 @@ const LectureList = () => {
           <input type="text" placeholder="원하는 강의를 검색하세요" />
         </div>
         <div className="keyword">
-          <select name="keyword">
-            <option value="1" selected>
-              키워드 찾기
-            </option>
-          </select>
+          <Select styles={customStyles} options={options} />
         </div>
       </SearchBox>
       <LectureItem />
