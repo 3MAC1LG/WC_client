ecfda747e67f84b1322f3e18926378bf710a65b5
JunSeo <oponize@naver.com>
2021-11-22 15:44:19 +0900

    [UI] Lecture Page Layout - Lecture

--- src/components/LectureItem/index.jsx
+++ src/components/LectureItem/index.jsx
@@ -1,17 +1,29 @@
+/** @jsxImportSource @emotion/react */
+import { useTheme } from '@emotion/react';
 import React from 'react';
+import { LectureItemContainer, LectureItemContainerStyles } from './styles';
 
-const index = () => {
+const LectureItem = ({ classroom }) => {
+  const theme = useTheme();
+  if (classroom) {
+    console.log(classroom);
+  }
   return (
-    <div>
-      <div>
-        <div></div>
-        <div>
-          <div></div>
-          <div></div>
+    <LectureItemContainer css={LectureItemContainerStyles(theme)}>
+      <div className="item-title">
+        <div className="item-title-left">
+          <p>{classroom.category}</p>
+          <h1>{classroom.title}</h1>
+        </div>
+        <div className="item-title-right">
+          <img
+            src={`http://localhost:4000/${classroom.thumbUrl}`}
+            alt="classThumb.png"
+          />
         </div>
       </div>
-    </div>
+    </LectureItemContainer>
   );
 };
 
-export default index;
+export default LectureItem;
---
+++ src/components/LectureItem/styles.jsx
@@ -0,0 +1,55 @@
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+
+export const LectureItemContainer = styled.div`
+  border-radius: 2rem;
+  .item-title {
+    display: flex;
+    padding: 3rem;
+    .item-title-left {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      p {
+        margin: 0;
+        font-size: 1.9rem;
+        font-weight: bold;
+        padding-bottom: 0.5rem;
+      }
+      h1 {
+        font-size: 2.4rem;
+      }
+    }
+    .item-title-right {
+      width: 9rem;
+      height: 9rem;
+      border-radius: 4.5rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
+        rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
+      img {
+        width: 70%;
+      }
+    }
+  }
+`;
+
+export const LectureItemContainerStyles = (theme) => css`
+  background: ${theme.border};
+  .item-title {
+    .item-title-left {
+      color: #080305;
+      p {
+      }
+      h1 {
+      }
+    }
+    .item-title-right {
+      background: #ffffff;
+      img {
+      }
+    }
+  }
+`;
